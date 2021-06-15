968a05f2985ca9d500427d1d95f94b285270bd39
sk2237 <suman77327@gmail.com>
2021-06-15 11:50:13 +0530

    debug updated

--- real_states/settings.py
+++ real_states/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dy4^8iuxhpx2f0*0+)y#!p$gy@%25x3oy$!g983+vb23$-zcp&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','sk-real-estate-management.herokuapp.com']
 
 
 # Application definition
